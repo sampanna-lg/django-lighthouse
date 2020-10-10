5291f2b4fb6acf9d300fe5c3133686aa6caea534
yukesh <nsiwansh@gmail.com>
2020-10-10 17:52:16 +0545

    postgres db and static root changed

--- SampannaLighthouse/settings.py
+++ SampannaLighthouse/settings.py
@@ -86,12 +86,25 @@ WSGI_APPLICATION = 'SampannaLighthouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mylighthouse',
+            'USER': 'mylighthouseuser',
+            'PASSWORD': 'admin1234',
+            'HOST' : 'localhost',
+            'PORT': '',
+        }
+    }
+
 #added for allauth
 # AUTHENTICATION_BACKENDS = [
     
